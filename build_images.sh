7e10968854b855b6820ca2c5d9e1502149913916
g.assies <gertjan.assies@gmail.com>
2018-10-23 12:54:39 +0200

    now pushes both version and latest tags seperately

--- build_images.sh
+++ build_images.sh
@@ -4,7 +4,9 @@ set -o nounset
 echo "Building yjp base images, make sure you have done a 'docker login' first"
 
 docker build . -f openjdk-8-jre/Dockerfile -t  thenewmotion/yjp-openjdk-8-jre:$VERSION -t thenewmotion/yjp-openjdk-8-jre:latest
+docker push thenewmotion/yjp-openjdk-8-jre:latest
 docker push thenewmotion/yjp-openjdk-8-jre:$VERSION
 
 docker build . -f openjdk-8-jre-alpine/Dockerfile -t thenewmotion/yjp-openjdk-8-jre-alpine:$VERSION -t thenewmotion/yjp-openjdk-8-jre-alpine:latest
+docker push thenewmotion/yjp-openjdk-8-jre-alpine:latest
 docker push thenewmotion/yjp-openjdk-8-jre-alpine:$VERSION
