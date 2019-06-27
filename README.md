f1f5bdee1d6f1e45cd9370249c5b7f4d58b51a05
g.assies <gertjan.assies@gmail.com>
2019-06-27 15:13:51 +0200

    Created Base Images for Java 11

--- README.md
+++ README.md
@@ -4,8 +4,28 @@ These images are openjdk images with the yourkit java profiler agent pre-install
 
 Using this image will start up the jvm with the agent enabled and exposed on port 10001
 
-Currently we support two openjdk images
- * openjdk:8-jre
- * openjdk:8-jre-alpine3.8
+# Currently images
+
+| Image                                  | Ver | Base Image               | Yourkit version |
+|----------------------------------------|-----|--------------------------|-----------------|
+| thenewmotion/yjp-openjdk-8-jre         | 1.3 | openjdk:8-jre            | 2018.04         |
+| thenewmotion/yjp-openjdk-11-jre        | 1.3 | openjdk:11-jre           | 2018.04         |
+| thenewmotion/yjp-openjdk-8-jre-alpine  | 1.3 | openjdk:8-jre-alpine3.8  | 2018.04         |
+| thenewmotion/yjp-openjdk-11-jre-alpine | 1.3 | openjdk:11-jre-alpine3.8 | 2018.04         |
 
 
+## Building images
+
+Make sure you are logged into docker with the tnmbuildagent docker hub accoubt
+
+Bash
+```
+VERSION=1.3 ./build_images.sh
+```
+
+Fish
+```
+env VERSION=1.3 ./build_images.sh
+```
+
+This will build the images and push them to the newmotion docker hub account
--- build_images.sh
+++ build_images.sh
@@ -1,7 +1,7 @@
 set -o errexit
 set -o nounset
 
-echo "Building yjp base images, make sure you have done a 'docker login' first"
+echo "Building yjp base images, make sure you have done a 'docker login' first and set the VERSION Env variable"
 
 docker build . -f openjdk-8-jre/Dockerfile -t  thenewmotion/yjp-openjdk-8-jre:$VERSION -t thenewmotion/yjp-openjdk-8-jre:latest
 docker push thenewmotion/yjp-openjdk-8-jre:latest
@@ -10,3 +10,11 @@ docker push thenewmotion/yjp-openjdk-8-jre:$VERSION
 docker build . -f openjdk-8-jre-alpine/Dockerfile -t thenewmotion/yjp-openjdk-8-jre-alpine:$VERSION -t thenewmotion/yjp-openjdk-8-jre-alpine:latest
 docker push thenewmotion/yjp-openjdk-8-jre-alpine:latest
 docker push thenewmotion/yjp-openjdk-8-jre-alpine:$VERSION
+
+docker build . -f openjdk-11-jre/Dockerfile -t  thenewmotion/yjp-openjdk-11-jre:$VERSION -t thenewmotion/yjp-openjdk-11-jre:latest
+docker push thenewmotion/yjp-openjdk-11-jre:latest
+docker push thenewmotion/yjp-openjdk-11-jre:$VERSION
+
+docker build . -f openjdk-11-jre-alpine/Dockerfile -t thenewmotion/yjp-openjdk-11-jre-alpine:$VERSION -t thenewmotion/yjp-openjdk-11-jre-alpine:latest
+docker push thenewmotion/yjp-openjdk-11-jre-alpine:latest
+docker push thenewmotion/yjp-openjdk-11-jre-alpine:$VERSION
